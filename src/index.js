e81f4426e9f1618c3cf23fd27aef0f4508e7be5e
seong su kim <know.warehouse02@gmail.com>
2024-09-16 19:24:41 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+// 리액트 사이트 전체를 렌더링해주는 핵심 파일

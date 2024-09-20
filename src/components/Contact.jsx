fe703a9097b5055c0d935df511704af795544f98
seong su kim <know.warehouse02@gmail.com>
2024-09-20 19:51:22 +0900

    contact 완료

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,58 @@
 import React from 'react';
 
+// 핵심 데이터 부품화
+const contactText = [
+  {
+    link: 'https://open.kakao.com/o/sCIpVY4f',
+    title: 'KAKAO : kim seong su',
+  },
+  {
+    link: 'mailto:know.warehouse02@gmail.com',
+    title: 'know.warehouse02@gmail.com',
+  },
+];
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section id="contact">
+      <div className="contact__inner">
+        <h2 className="contact__title">Contact</h2>
+        <div className="contact__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="contact__text">
+          <div className="text">
+            {contactText.map((contact, key) => (
+              <div key={key}>
+                <a
+                  href={contact.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {contact.title}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="contact__lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
